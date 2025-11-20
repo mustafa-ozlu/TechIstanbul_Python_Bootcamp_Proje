907fcf07cd32eca6dc578690809498796e77d279
Mustafa Özlü <mustafa09@yahoo.com>
2025-11-20 14:40:03 +0300

    Change Flask app port to 5000
    Updated the port for the Flask application to 5000.

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def get_weather():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0", port=8000)
+
+    app.run(debug=True,host="0.0.0.0", port=5000)
